09f9bcd7645b6a5c148503d854b77dd34b7e9ff7
myHFC <122330374+myhfc@users.noreply.github.com>
2023-01-21 03:46:46 +0100

    update run.py

--- run.py
+++ run.py
@@ -2,5 +2,6 @@ from  package.web_app_flask import app
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT',5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run()
+    """port = int(os.environ.get('PORT',5000))
+    app.run(host="0.0.0.0", port=port, debug=True)"""
