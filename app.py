f6277801b568095ca90377e8326bda2540004f31
myHFC <122330374+myhfc@users.noreply.github.com>
2023-02-10 16:47:03 +0100

    Update app.py clean code

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-import os
-import pandas as pd
-#import azure.functions as func
-from flask import (Flask, render_template )#,request, url_for, send_from_directory)
+import requests
+from flask import (Flask, render_template ,request)#, url_for, send_from_directory)
 
 app = Flask(__name__)
 
 # Home Page
 @app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html', predict=False)
+    if request.method=="POST":
+        userid = request.form["user_id"]
+        print(userid)
+        article_score = requests.get("https://oc-09-function-02.azurewebsites.net/api/HttpTrigger1", params={"userid":userid})
+        recommended_articles = str(article_score.content)[2:-2]
+        list_recommended_articles = recommended_articles.split(sep=",")
+        
+        return render_template('index.html', predict=True, userId=userid, recommended_articles=list_recommended_articles)
+    else:
+        return render_template('index.html', predict=False)
 
 # Hello Page
 @app.route("/hello/<name>", methods=['GET'])
